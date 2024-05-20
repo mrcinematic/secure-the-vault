2c9255e4602725a1f48881d800aa7d8d6ea84b4e
Daniel <danielgalt@yahoo.com>
2024-05-20 08:11:49 -0600

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,15 @@
+// Pseudocode:
+// 1. Calculate the first code.
+const num1 = (5 + 5);
+// 2. Calculate the second code.
+const num2 = (10 * 4);
+// 3. Calculate the third code.
+const num3 = (40 - 1);
+// 4. Enter the combination.
+// 5. Open the vault.
+
+// STRING
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+// Combination:
+console.log(`The combination is: ${num1} - ${num2} - ${num3}`);
